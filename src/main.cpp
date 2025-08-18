91110b48eb8c37d59eb219321b75883e22597d8c
Kevin James Baxter <meta.data@verizon.net>
2025-08-18 19:33:07 -0400

    Initiaal commit & puah.

---
+++ src/main.cpp
@@ -0,0 +1,107 @@
+
+
+/**********************************************************************
+  Filename    : SDMMC Test
+  Description : The SD card is accessed using the SDMMC one-bit bus
+  Auther      : www.freenove.com
+  Modification: 2022/10/28
+**********************************************************************/
+
+#include <SPI.h>
+#include <SD.h>
+
+//#include "sd_read_write.h"
+#include "SD_MMC.h"
+
+#define SD_MMC_CMD 38 //Please do not modify it.
+#define SD_MMC_CLK 39 //Please do not modify it. 
+#define SD_MMC_D0  40 //Please do not modify it.
+
+
+void printDirectory(File dir, int numTabs);
+
+
+void setup() {
+    Serial.begin(115200);
+    SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+    if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
+      Serial.println("Card Mount Failed");
+      return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+    if(cardType == CARD_NONE){
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+    Serial.print("SD_MMC Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+    Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+
+    //listDir(SD_MMC, "/", 0);
+
+    //createDir(SD_MMC, "/mydir");
+    //listDir(SD_MMC, "/", 0);
+
+    //removeDir(SD_MMC, "/mydir");
+    //listDir(SD_MMC, "/", 2);
+
+    //writeFile(SD_MMC, "/hello.txt", "Hello ");
+    //appendFile(SD_MMC, "/hello.txt", "World!\n");
+    //readFile(SD_MMC, "/hello.txt");
+
+    //deleteFile(SD_MMC, "/foo.txt");
+    //renameFile(SD_MMC, "/hello.txt", "/foo.txt");
+    //readFile(SD_MMC, "/foo.txt");
+
+    //testFileIO(SD_MMC, "/test.txt");
+
+    Serial.printf("Total space: %lluMB\r\n", SD_MMC.totalBytes() / (1024 * 1024));
+    Serial.printf("Used space: %lluMB\r\n", SD_MMC.usedBytes() / (1024 * 1024));
+
+    File dir = SD_MMC.open("/");
+    printDirectory(dir, 0);
+
+    Serial.println("*finis*");
+  }
+
+void loop() {
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+
+
+
+
+void printDirectory(File dir, int numTabs) {
+   while (true) {
+
+      File entry = dir.openNextFile();
+      if (! entry) {
+         // no more files
+         break;
+      }
+      for (uint8_t i = 0; i < numTabs; i++) {
+         Serial.print('\t');
+      }
+      Serial.print(entry.name());
+      if (entry.isDirectory()) {
+         Serial.println("/");
+         printDirectory(entry, numTabs + 1);
+      } else {
+         // files have sizes, directories do not
+         Serial.print("\t\t");
+         Serial.println(entry.size(), DEC);
+      }
+      entry.close();
+   }
+}
